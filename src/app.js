a4c017ae878955978d62d0213fb778f82957631a
javs199 <ch.jesus.99@gmail.com>
2026-01-28 18:54:28 -0600

    setup

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API running' });
+});
+
+export default app;
